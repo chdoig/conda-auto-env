37c882d86f45028e6dc11414c39daefa6dac5c24
Cenny Wenner <cwenner@gmail.com>
2016-06-11 16:47:07 +0200

    Specify check of active environment
    [fix] More accurate check of whether an environment is
    already active. Environments could previously be seen
    as active by certain spurious directory or environment
    names.

--- tmp.sh
+++ tmp.sh
@@ -12,11 +12,31 @@
 #
 
 function conda_auto_env() {
+  if [ -e "env.yml" ]; then
+    #echo "env.yml file found"
+    ENV=$(head -n 1 env.yml | cut -f2 -d ' ')
+    # Check if you are already in the environment
+    #echo $ENV
+    if [[ $PATH != *$ENV* ]]; then
+      # Check if the environment exists
+      #echo $ENV
+      source activate $ENV
+      if [ $? -eq 0 ]; then
+        echo "activated"
+        :
+      else
+        # Create the environment and activate
+        echo "Conda env '$ENV' doesn't exist."
+        conda env create -q
+        source activate $ENV
+      fi
+    fi
+  fi
   if [ -e "environment.yml" ]; then
     # echo "environment.yml file found"
     ENV=$(head -n 1 environment.yml | cut -f2 -d ' ')
     # Check if you are already in the environment
-    if [[ $PATH != */envs/*$ENV*/* ]]; then
+    if [[ $PATH != *$ENV* ]]; then
       # Check if the environment exists
       source activate $ENV
       if [ $? -eq 0 ]; then
@@ -31,4 +51,9 @@ function conda_auto_env() {
   fi
 }
 
-export PROMPT_COMMAND=conda_auto_env
+if [ $(ps -p$$ -ocmd=) "=" "zsh" ]
+then
+  precmd() { conda_auto_env; }
+else
+  export PROMPT_COMMAND=conda_auto_env
+fi
