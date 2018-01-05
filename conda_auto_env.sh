7ecb693b8699f0e9e0c163fa2aa4231261d077f8
Dror Atariah <d.atariah@rebuy.com>
2018-01-05 09:07:41 +0100

    Fixed and annotated toggling

--- conda_auto_env.sh
+++ conda_auto_env.sh
@@ -11,24 +11,29 @@
 #       source /path/to/conda_auto_env.sh
 #
 
+ACTIVATED="${ENV} activated!"
+DEACTIVATE="${ENV} is already activated. Deactivating..."
+
 function conda_auto_env() {
   if [ -e "environment.yml" ]; then
-    # echo "environment.yml file found"
+    echo "environment.yml file found"
     ENV=$(head -n 1 environment.yml | cut -f2 -d ' ')
     CURRENT_ENV=$(conda env list | grep \* | cut -f 1 -d " ")
     # Check if you are already in the environment
     if [ $ENV = $CURRENT_ENV ]; then
+      echo $DEACTIVATE
       conda deactivate
     elif [[ $PATH != *$ENV* ]]; then
       # Check if the environment exists
       conda activate $ENV
       if [ $? -eq 0 ]; then
-        :
+        echo $ACTIVATED
       else
         # Create the environment and activate
         echo "Conda env '$ENV' doesn't exist."
         conda env create -q
         conda activate $ENV
+        echo $ACTIVATED
       fi
     fi
   fi
