42e87d298795672178b3ecf77084394979b4e5cf
Dror Atariah <d.atariah@rebuy.com>
2018-01-12 08:35:03 +0100

    Initial attempt to act based on dir name
    If environment.yml doesn't exist

--- conda_auto_env.sh
+++ conda_auto_env.sh
@@ -11,29 +11,50 @@
 #       source /path/to/conda_auto_env.sh
 #
 
+DEFAULT_PY_VER=3.6
+
 function conda_auto_env() {
+  # Determine ENV's name; eitehr from environment.yml
+  # or based on current dir name
   if [ -e "environment.yml" ]; then
     echo "environment.yml file found"
     ENV=$(head -n 1 environment.yml | cut -f2 -d ' ')
-    ACTIVATED="${ENV} activated!"
-    DEACTIVATE="${ENV} is already activated. Deactivating..."
-    CURRENT_ENV=$(conda env list | grep \* | cut -f 1 -d " ")
-    # Check if you are already in the environment
-    if [ $ENV = $CURRENT_ENV ]; then
-      echo $DEACTIVATE
-      conda deactivate
-    elif [[ $PATH != *$ENV* ]]; then
-      # Check if the environment exists
-      conda activate $ENV
-      if [ $? -eq 0 ]; then
-        echo $ACTIVATED
-      else
-        # Create the environment and activate
-        echo "Conda env '$ENV' doesn't exist."
+  else
+    echo "No environment.yml found. Reverting to current dir"
+    ENV=${PWD##*/}
+  fi
+
+  echo "Processing the environment: ${ENV}"
+
+  ACTIVATED="${ENV} activated!"
+  DEACTIVATE="${ENV} is already activated. Deactivating..."
+  CURRENT_ENV=$(conda env list | grep \* | cut -f 1 -d " ")
+
+  if [ $ENV = $CURRENT_ENV ]; then
+    # ENV is already activated. Deactivate
+    echo $DEACTIVATE
+    conda deactivate
+    if [[ $? -eq 0 ]]; then
+      echo "Deactivated successfully"
+    else
+      echo "Failed to deactivate"
+    fi
+  elif [[ $PATH != *$ENV* ]]; then
+    # ENV is not activated. Trying to activate
+    conda activate $ENV
+    if [ $? -eq 0 ]; then
+      echo $ACTIVATED
+    else
+      # Failed to activate. Creating environment
+      echo "Conda env '$ENV' doesn't exist."
+      if [[ -e "environment.yml" ]]; then
         conda env create -q
-        conda activate $ENV
-        echo $ACTIVATED
+      else
+        echo "Creating ${ENV} with default settings"
+        conda create -n ${ENV} python=${DEFAULT_PY_VER}
       fi
+      conda activate $ENV
+      echo $ACTIVATED
     fi
   fi
 }
