df0550c3ae805a84137e9997b4cf69d97050bf47
Dror Atariah <drorata@gmail.com>
2018-01-16 11:30:12 +0100

    Improved calls and return values checks

--- conda_auto_env.sh
+++ conda_auto_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# conda-auto-env automatically activates a conda environment when
-# entering a folder with an environment.yml file.
+# conda-auto-env is a utility to activate a conda environment in a folder
+# with an environment.yml file or based on the current dir's name.
 #
 # If the environment doesn't exist, conda-auto-env creates it and
 # activates it for you.
@@ -14,10 +14,31 @@
 DEFAULT_PY_VER=3.6
 
 function conda_auto_env() {
+
+  function activate_env() {
+    conda activate $1
+    if [[ $? -eq 0 ]]; then
+      echo "${1} activated successfully"
+    else
+      echo "Failed to activate ${1}"
+      return 1
+    fi
+  }
+
+  function deactivate_env() {
+    conda deactivate
+    if [[ $? -eq 0 ]]; then
+      echo "Deactivated successfully"
+    else
+      echo "Failed to deactivate"
+      return 1
+    fi
+  }
+
   # Determine ENV's name; eitehr from environment.yml
   # or based on current dir name
   if [ -e "environment.yml" ]; then
-    echo "environment.yml file found"
+    echo "Found environment.yml"
     ENV=$(head -n 1 environment.yml | cut -f2 -d ' ')
   else
     echo "No environment.yml found. Reverting to current dir"
@@ -26,35 +47,29 @@ function conda_auto_env() {
 
   echo "Processing the environment: ${ENV}"
 
-  ACTIVATED="${ENV} activated!"
-  DEACTIVATE="${ENV} is already activated. Deactivating..."
   CURRENT_ENV=$(conda env list | grep \* | cut -f 1 -d " ")
 
   if [ $ENV = $CURRENT_ENV ]; then
     # ENV is already activated. Deactivate
-    echo $DEACTIVATE
-    conda deactivate
-    if [[ $? -eq 0 ]]; then
-      echo "Deactivated successfully"
-    else
-      echo "Failed to deactivate"
-    fi
+    deactivate_env
   elif [[ $PATH != *$ENV* ]]; then
     # ENV is not activated. Trying to activate
-    conda activate $ENV
-    if [ $? -eq 0 ]; then
-      echo $ACTIVATED
-    else
+    activate_env $ENV
+    if [ $? -ne 0 ]; then
       # Failed to activate. Creating environment
       echo "Conda env '$ENV' doesn't exist."
       if [[ -e "environment.yml" ]]; then
+        echo "Creating ${ENV} based on environment.yml"
         conda env create -q
       else
         echo "Creating ${ENV} with default settings"
         conda create -n ${ENV} python=${DEFAULT_PY_VER}
       fi
-      conda activate $ENV
-      echo $ACTIVATED
+      activate_env $ENV
     fi
   fi
+
+  # Make sure utility functions are local
+  unset -f activate_env
+  unset -f deactivate_env
 }
