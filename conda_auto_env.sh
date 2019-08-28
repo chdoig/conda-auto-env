de9c1028b3a923d4df1b36bf75c0c85f1e20df55
Cenny <cwenner@gmail.com>
2019-08-28 11:34:13 +0200

    Update to use conda command instead of source

--- conda_auto_env.sh
+++ conda_auto_env.sh
@@ -19,14 +19,14 @@ function conda_auto_env() {
     if [[ $PATH != */envs/*$ENV*/* ]]; then
       # Attempt to activate environment.
       CONDA_ENVIRONMENT_ROOT="" #For spawned shells
-      source activate $ENV
+      conda activate $ENV
       # Set root directory of active environment.
       CONDA_ENVIRONMENT_ROOT="$(pwd)"
       if [ $? -ne 0 ]; then
         # Create the environment and activate.
         echo "Conda environment '$ENV' doesn't exist: Creating."
         conda env create -q
-        source activate $ENV
+        conda activate $ENV
       fi
     fi
   # Deactivate active environment if we are no longer among its subdirectories.
@@ -35,7 +35,7 @@ function conda_auto_env() {
     && [[ $(pwd) != $CONDA_ENVIRONMENT_ROOT/* ]]
   then
     CONDA_ENVIRONMENT_ROOT=""
-    source deactivate
+    conda deactivate
   fi
 }
 
